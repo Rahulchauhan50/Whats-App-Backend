6ea579af4faf1359c82f2e55898aef6015f3d372
Rahul chauhan <rahul@Rahuls-MacBook-Pro.local>
2024-02-15 10:48:03 +0530

    15 feb

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const server = app.listen(port,()=>{
 
 const io = new Server(server, {
     cors:{
-        origin:"http://localhost:3000",
+        origin:"https://whatsapw.netlify.app/",
     },
 })
 
