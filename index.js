14ee01f87ba0fe34ea9f6b7a39ebb582ec5b7e3e
Rahul chauhan <rahulchauhah50@gmail.com>
2026-03-23 18:34:39 +0530

    tast api added

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use("/uploads/recordings", express.static("uploads/recordings"));
 app.use("/uploads/documents", express.static("uploads/documents"));
 
 app.get("/api/test", (req, res) => {
-  res.json({ status: "ok", message: "API is working" });
+  res.json({ status: "ok", message: "API is working 2" });
 });
 
 app.use("/api/auth",AuthRoutes)
