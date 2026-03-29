c800e4e15f2648fa91e6fbbf146220332dc7b722
Rahul chauhan <rahulchauhah50@gmail.com>
2026-03-29 14:29:49 +0530

    index.js changed

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.use("/uploads/recordings", express.static("uploads/recordings"));
 app.use("/uploads/documents", express.static("uploads/documents"));
 
 app.get("/api/test", (req, res) => {
-    res.json({ status: "ok", message: "API is working v1" });
+    res.json({ status: "ok", message: "API is working v11" });
 });
 
 app.use("/api/auth", AuthRoutes)
